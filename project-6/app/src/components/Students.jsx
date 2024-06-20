1e59fde17bb17aecfe446f3257729cdec2c27ec5
sabaree-88 <besabareesh@gmail.com>
2024-06-20 08:02:31 +0530

    project-6

---
+++ project-6/app/src/components/Students.jsx
@@ -0,0 +1,12 @@
+const Students = () => {
+    return (
+      <>
+        <div className="flex justify-center items-center bg-stone-900 h-[100vh] w-full">
+          <div className="w-1/2 bg-white rounded-sm h-[50vh] p-5"><h1 className="text-black font-sans text-2xl">Hi</h1></div>
+        </div>
+      </>
+    );
+  };
+  
+  export default Students;
+  
