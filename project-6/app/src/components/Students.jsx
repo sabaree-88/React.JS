f4e84046bb686d2ee8cb2f26f4065300c73299eb
sabaree-88 <besabareesh@gmail.com>
2024-06-20 08:41:13 +0530

    table

--- project-6/app/src/components/Students.jsx
+++ project-6/app/src/components/Students.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Students = () => {
+  useEffect(()=>{
+    fetch("http://localhost:8081/user")
+    .then((res)=> res)
+    .then((data) => data.json())
+    .catch(err => console.error(err))
+  },[])
   return (
     <>
       <div className="flex justify-center items-center bg-stone-900 h-[100vh] w-full">
@@ -9,24 +16,27 @@ const Students = () => {
             Add Student
           </Link>
 
-          <div class="relative overflow-x-auto mt-4">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-gray-900 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400">
+          <div className="relative overflow-x-auto mt-4">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-900 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" class="px-6 py-3">
+                  <th scope="col" className="px-6 py-3">
                     ID
                   </th>
-                  <th scope="col" class="px-6 py-3">
+                  <th scope="col" className="px-6 py-3">
                     Name
                   </th>
-                  <th scope="col" class="px-6 py-3">
+                  <th scope="col" className="px-6 py-3">
                     Email
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Action
                   </th>
                 </tr>
               </thead>
               <tbody>
                 {
-                  
+
                 }
               </tbody>
             </table>
